6754b095742df909370c04dfe21f6ccfadb8c496
ilhan cuvelek <aangrava1903@gmail.com>
2022-08-22 18:11:28 +0300

    admin add product

--- ShopApp/Controllers/AdminController.cs
+++ ShopApp/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.Abstract;
+using Entity;
 using Microsoft.AspNetCore.Mvc;
 using ShopApp.Models;
 
@@ -18,6 +19,25 @@ namespace ShopApp.Controllers
                 Products = _productRepository.GetAll()
             };
             return View(productListViewModel);
+        }
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateProduct(ProductModel productModel)
+        {
+            var entity = new Product()
+            {
+                Price = productModel.Price,
+                Name=productModel.Name,
+                Description=productModel.Description,
+                ImageUrl=productModel.ImageUrl,
+                Url=productModel.Url    
+            };
+            _productRepository.Create(entity);
+            return RedirectToAction("ProductList");
         }
     }
 }
---
+++ ShopApp/Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopApp.Models
+{
+
+    //entity deki lerin hepsini kullanmak istemediğimizde veya
+    //değişiklik yapacağımız zaman entity de yapmayalım diye oluşturduk.
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Url { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
