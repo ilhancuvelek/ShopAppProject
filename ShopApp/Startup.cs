467f183efc0b5d68f678e8309a5dff9e77ec772a
ilhan cuvelek <aangrava1903@gmail.com>
2022-09-13 12:42:43 +0300

    identity settings

--- ShopApp/Startup.cs
+++ ShopApp/Startup.cs
@@ -5,11 +5,15 @@ using DataAccess.Concrete;
 using DataAccess.Concrete.EfCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ShopApp.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +34,45 @@ namespace ShopApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            //Identity tablosu için
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlite("Data Source=shopAppProjectDb"));
+            services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<ApplicationContext>().AddDefaultTokenProviders();
+            
+            //Identity ayarlarý için
+            services.Configure<IdentityOptions>(options => {
+                // password
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = true;
+
+                // Lockout                
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.AllowedForNewUsers = true;
+
+                // options.User.AllowedUserNameCharacters = "";
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+            });
+
+            services.ConfigureApplicationCookie(options => {
+                options.LoginPath = "/account/login";
+                options.LogoutPath = "/account/logout";
+                options.AccessDeniedPath = "/account/accessdenied";
+                options.SlidingExpiration = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+                options.Cookie = new CookieBuilder
+                {
+                    HttpOnly = true,
+                    Name = ".ShopApp.Security.Cookie"
+                };
+            });
+            //Identity ayarlarý için --SON--
+
             services.AddScoped<IProductRepository,EfCoreProductRepository>();
             services.AddScoped<ICategoryRepository, EfCoreCategoryRepository>();
 
@@ -55,6 +98,9 @@ namespace ShopApp
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            //Identity tablosu için
+            app.UseAuthentication();
 
             app.UseRouting();
 
